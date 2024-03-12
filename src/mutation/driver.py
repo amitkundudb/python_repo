bcc89f8df5a3b91d9d6ed16913ddffe37dc216c9
amitkundudb <amit.kundu@diggibyte.com>
2024-03-12 18:50:38 +0530

    mutation by replaces the character at the specified position in the string

--- src/mutation/driver.py
+++ src/mutation/driver.py
@@ -1,6 +1,6 @@
-from src.mutation.utils import *
-
 import logging
+from src.mutation.utils import mutate_string_and_return
+
 logging.basicConfig(level=logging.INFO,format='%(message)s')
 if __name__ == '__main__':
     string = input()
@@ -8,3 +8,4 @@ if __name__ == '__main__':
     character = input()
     mutated_string = mutate_string_and_return(string, position, character)
     logging.info(mutated_string)
+
