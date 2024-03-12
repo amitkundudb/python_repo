98e5c0bfcff98f613f03a90295d94d8db608503e
amitkundudb <amit.kundu@diggibyte.com>
2024-03-12 16:29:15 +0530

    This is a utils file for marks_percentage

--- src/marks_percentage/utils.py
+++ src/marks_percentage/utils.py
@@ -1,17 +1,12 @@
-import logging
 
-def read_student_marks():
-    n = int(input().strip())
+
+if __name__ == '__main__':
+    n = int(input())
     student_marks = {}
     for _ in range(n):
-        name, *line = input().strip().split()
+        name, *line = input().split()
         scores = list(map(float, line))
         student_marks[name] = scores
-    return student_marks
-
-def calculate_average_marks(student_marks, query_name):
-    if query_name in student_marks:
-        avg_marks = sum(student_marks[query_name]) / len(student_marks[query_name])
-        return avg_marks
-    else:
-        logging.error("Student name not found in records")
+    query_name = input()
+    avg_marks = (sum(student_marks[query_name]))/3
+    print(f"{avg_marks:.2f}")
