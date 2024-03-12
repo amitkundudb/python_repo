9ba96023f687b0f42d1e89a441f9f54cde694bee
amitkundudb <amit.kundu@diggibyte.com>
2024-03-12 16:40:39 +0530

    This is an utils file for marks_percentage

--- src/marks_percentage/utils.py
+++ src/marks_percentage/utils.py
@@ -1,12 +1,34 @@
+#In this problem we are taking input of marks of students
+#In the Result We are getting the average of marks of desired student
+#I have also considered incase any error happens we will get a error message
 
-
-if __name__ == '__main__':
-    n = int(input())
+import logging
+logging.basicConfig(level=logging.INFO,format='%(message)s')
+def read_student_marks():
+    n = int(input().strip())
     student_marks = {}
     for _ in range(n):
-        name, *line = input().split()
+        name, *line = input().strip().split()
         scores = list(map(float, line))
         student_marks[name] = scores
-    query_name = input()
-    avg_marks = (sum(student_marks[query_name]))/3
-    print(f"{avg_marks:.2f}")
+    return student_marks
+
+def calculate_average_marks(student_marks, query_name):
+    if query_name in student_marks:
+        avg_marks = sum(student_marks[query_name]) / len(student_marks[query_name])
+        return avg_marks
+    else:
+        logging.error("Student name not found in records")
+def average_marks():
+    # Read student marks
+    student_marks = read_student_marks()
+
+    # Read query name
+    query_name = input().strip()
+
+    # Calculate average marks
+    avg_marks = calculate_average_marks(student_marks, query_name)
+    if avg_marks:
+        logging.info(f"{avg_marks:.2f}")
+    else:
+        logging.error("Failed to calculate average marks")
