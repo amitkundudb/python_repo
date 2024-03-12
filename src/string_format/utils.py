5e154361dae4af98c513b9db0c5c3e7942264ce9
amitkundudb <amit.kundu@diggibyte.com>
2024-03-12 23:23:02 +0530

    decimal,octal,hexadecimal,and binary representations of integer

---
+++ src/string_format/utils.py
@@ -0,0 +1,13 @@
+import logging
+
+def string_format(number):
+    width = len(bin(number)[2:])
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
+    for i in range(1, number + 1):
+        decimal = str(i).rjust(width)
+        octal = oct(i)[2:].rjust(width)
+        hexadecimal = hex(i)[2:].upper().rjust(width)
+        binary = bin(i)[2:].rjust(width)
+        logging.info(f"{decimal} {octal} {hexadecimal} {binary}")
+# Taking user input
+number = int(input())
