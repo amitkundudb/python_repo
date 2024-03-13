909cb1aee8f1e0a844f73f136d679b43de63ef6e
amitkundudb <amit.kundu@diggibyte.com>
2024-03-13 14:06:11 +0530

    floor,ceil,rint using numpy library

---
+++ src/floor_ceil_rint/utils.py
@@ -0,0 +1,10 @@
+import numpy
+import logging
+
+logging.basicConfig(level=logging.INFO,format='%(message)s')
+def floorceilrent():
+    numpy.set_printoptions(legacy='1.13')
+    A = numpy.array(list(map(float, input().split())))
+    logging.info(f"{numpy.floor(A)}")
+    logging.info(f"{numpy.ceil(A)}")
+    logging.info(f"{numpy.rint(A)}")
