93fba8728b04599d61b94d3fd53d080cd58dea53
amitkundudb <amit.kundu@diggibyte.com>
2024-03-13 14:41:09 +0530

    Determinant of a square matrix using numpy

---
+++ src/linear_algebra/utils.py
@@ -0,0 +1,17 @@
+"""Find the determinant of a square matrix using numpy
+numpy.linalg.det([[1 , 2], [2, 1]])
+linalg.det is used to find determinant"""
+
+
+import numpy
+import logging
+logging.basicConfig(level=logging.INFO,format='%(message)s')
+
+def linearalgebra():
+    mtx_size = int(input())
+    mtx_a = []
+    for _ in range(mtx_size):
+        row = list(map(float, input().split()))
+        mtx_a.append(row)
+    mtx_det = round(numpy.linalg.det(mtx_a), 2)
+    logging.info(mtx_det)
