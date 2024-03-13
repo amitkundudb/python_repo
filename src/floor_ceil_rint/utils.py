3bf461ea287c2bb45b0f905b3e29d3f21ace035d
amitkundudb <amit.kundu@diggibyte.com>
2024-03-13 14:10:30 +0530

    floor,ceiling,and rint using numpy

--- src/floor_ceil_rint/utils.py
+++ src/floor_ceil_rint/utils.py
@@ -1,3 +1,6 @@
+# This code imports the NumPy library,
+# sets print options, takes user input for an array,
+# applies floor, ceiling, and rint functions to the array,
 import numpy
 import logging
 
