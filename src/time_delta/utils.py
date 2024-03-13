42fb1a77b74057b4270257f9530a278a2920d16f
amitkundudb <amit.kundu@diggibyte.com>
2024-03-13 13:11:21 +0530

    Calculating the time difference in seconds

---
+++ src/time_delta/utils.py
@@ -0,0 +1,20 @@
+import datetime
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+
+def time_delta():
+    num_test_cases = int(input())
+    for _ in range(num_test_cases):
+        t1_str = input()
+        t2_str = input()
+
+        # Parse the timestamps into datetime objects
+        t1 = datetime.datetime.strptime(t1_str, "%a %d %b %Y %H:%M:%S %z")
+        t2 = datetime.datetime.strptime(t2_str, "%a %d %b %Y %H:%M:%S %z")
+
+        # Calculate the absolute difference in seconds
+        delta = abs(t1 - t2).total_seconds()
+        result = int(delta)  # Convert to integer for the final result
+        logging.info(result)
