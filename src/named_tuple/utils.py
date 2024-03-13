733c06127c134f116faf9a905bd9b5e501e1abbd
amitkundudb <amit.kundu@diggibyte.com>
2024-03-13 10:45:00 +0530

    calculates the average marks of students using a namedtuple

---
+++ src/named_tuple/utils.py
@@ -0,0 +1,10 @@
+#calculates the average marks of students using a namedtuple
+import logging
+from collections import namedtuple
+
+logging.basicConfig(level=logging.INFO,format='%(message)s')
+def named_tuple():
+    N = int(input())
+    Sheet = namedtuple('Sheet', input().split())
+    marks= [Sheet._make(input().split()).MARKS for i in range(N)]
+    logging.info(sum(map(int, marks))/N)
