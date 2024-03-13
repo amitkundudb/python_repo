90b504f1cadc907694b5915224e90b5760c1b6de
amitkundudb <amit.kundu@diggibyte.com>
2024-03-13 14:22:03 +0530

    Compute min along axis 1 & o/p max of that result

---
+++ src/min_max/utils.py
@@ -0,0 +1,13 @@
+#Given a 2-D array with dimensions N*M
+#Task is to perform the min function over axis  and then find the max of that
+
+import numpy as np
+import logging
+
+logging.basicConfig(level=logging.INFO,format='%(message)s')
+def minmax():
+    N, i = input().split()
+    N = int(N)
+
+    arr = [list(map(int, input().split())) for i in range(N)]
+    logging.info(np.max(np.min(np.array(arr), axis=1)))
