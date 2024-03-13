f98dd4e043413964f741ef884666568c402c52ba
amitkundudb <amit.kundu@diggibyte.com>
2024-03-13 16:23:55 +0530

    uniform probability from the list

---
+++ src/iterators_iterables/utils.py
@@ -0,0 +1,22 @@
+'''You are given a list of  lowercase English letters.
+ For a given integer ,
+ select any  indices (assume -based indexing)
+  with a uniform probability from the list.'''
+
+from itertools import combinations
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+
+def iterables_iterators():
+    N = int(input())
+    l = input().split()
+    K = int(input())
+    newl = list(combinations(l, K))
+    length = 0
+    for i in newl:
+        if 'a' in i:
+            length += 1
+    ratio = length / len(newl)
+    logging.info(round(ratio, 6))
