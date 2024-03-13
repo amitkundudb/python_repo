b02a8e02187366e2d6993c4d321e85304e7b265e
amitkundudb <amit.kundu@diggibyte.com>
2024-03-13 10:22:45 +0530

    mutation by replace character at specified position in string

--- src/mutation/utils.py
+++ src/mutation/utils.py
@@ -4,14 +4,12 @@
 import logging
 
 
-def mutate_string_and_return(string, position, character):
+def mutate_string_and_return():
+    string = input()
+    position = int(input())
+    character = input()
     logging.basicConfig(level=logging.INFO, format='%(message)s')
     l = list(string)
     l[position] = character
     string = ''.join(l)
-    return string
-
-
-string = input()
-position = int(input())
-character = input()
+    logging.info(string)
